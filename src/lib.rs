1a040e465cb40b873dd26396d024d9a37c4676ef
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-06 22:47:58 +0600

    refactored a little bit

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,123 @@ extern crate serde_json;
 
 use reqwest::Client;
 use serde::{Serialize, Deserialize};
+use std::env;
+
+/*
+-h or --help: Display help message
+-v or --version: Display version
+-m or --message: Message to send to the model
+-i or --imagine: Generate image from text
+*/
+
+pub enum Command {
+    Help,
+    Version,
+    Message(String),
+    Imagine(String),
+}
+
+pub struct Config {
+    pub command: Command,
+}
+
+impl Config {
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let command = match args.next() {
+            Some(arg) => {
+                match arg.as_str() {
+                    "-h" | "--help" => Command::Help,
+                    "-v" | "--version" => Command::Version,
+                    "-m" | "--message" => {
+                        let message = match args.next() {
+                            Some(message) => message,
+                            None => return Err("No message provided"),
+                        };
+                        Command::Message(message)
+                    },
+                    "-i" | "--imagine" => {
+                        let message = match args.next() {
+                            Some(message) => message,
+                            None => return Err("No message provided"),
+                        };
+                        Command::Imagine(message)
+                    },
+                    _ => return Err("Invalid flag"),
+                }
+            },
+            None => return Err("No flag provided"),
+        };
+
+        Ok(Config { command })
+    }
+}
+
+pub struct LLMClient {
+    pub host: String,
+    pub model: String,
+    pub api_key: String,
+}
+
+impl LLMClient {
+    pub fn new(host: String, model: String, api_key: String) -> Result<Self, &'static str> {
+        Ok(LLMClient {
+            host,
+            model,
+            api_key,
+        })
+    }
+
+    pub async fn run(&self, query: &str) {
+        let query = query.trim();
+        let url = format!("{}/chat/completions", self.host);
+        let data = format!("{{\"model\": \"{}\",\"messages\": [{{\"role\": \"system\",\"content\": \"You are a helpful assistant.\"}},{{\"role\": \"user\",\"content\": \"{}\"}}]}}", self.model, query);
+        let client = Client::new();
+        let res = client.post(&url)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.api_key))
+            .body(data)
+            .send()
+            .await;
+
+        match res {
+            Ok(res) => {
+                let body = res.text().await.unwrap();
+                let completion: Completion = serde_json::from_str(&body).unwrap();
+                println!("{}", completion.choices[0].message.content);
+            }
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
+    }
+}
+
+pub async fn run(config: Config) {
+    let client = LLMClient::new(
+        env::var("LLM_HOST").unwrap(),
+        env::var("LLM_MODEL").unwrap(),
+        env::var("LLM_API_KEY").unwrap(),
+    ).unwrap_or_else(|err| {
+        panic!("{}", err);
+    });
+
+    match config.command {
+        Command::Help => {
+            println!("Help message");
+        },
+        Command::Version => {
+            println!("Version message");
+        },
+        Command::Message(message) => {
+            let _ = client.run(&message).await;
+        },
+        Command::Imagine(message) => {
+            println!("Imagine message: {}", message);
+        },
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct Completion {
@@ -47,44 +164,4 @@ struct Usage {
     prompt_tokens: i32,
     completion_tokens: i32,
     total_tokens: i32,
-}
-
-pub struct LLMClient {
-    pub host: String,
-    pub model: String,
-    pub api_key: String,
-}
-
-impl LLMClient {
-    pub fn new(host: String, model: String, api_key: String) -> Result<Self, &'static str> {
-        Ok(LLMClient {
-            host,
-            model,
-            api_key,
-        })
-    }
-
-    pub async fn run(&self, query: &str) {
-        let query = query.trim();
-        let url = format!("{}/chat/completions", self.host);
-        let data = format!("{{\"model\": \"{}\",\"messages\": [{{\"role\": \"system\",\"content\": \"You are a helpful assistant.\"}},{{\"role\": \"user\",\"content\": \"{}\"}}]}}", self.model, query);
-        let client = Client::new();
-        let res = client.post(&url)
-            .header("Content-Type", "application/json")
-            .header("Authorization", format!("Bearer {}", self.api_key))
-            .body(data)
-            .send()
-            .await;
-
-        match res {
-            Ok(res) => {
-                let body = res.text().await.unwrap();
-                let completion: Completion = serde_json::from_str(&body).unwrap();
-                println!("{}", completion.choices[0].message.content);
-            }
-            Err(err) => {
-                println!("{}", err);
-            }
-        }
-    }
 }
