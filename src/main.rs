8d76ebb478c766838f631299a907a5eaa313ebbe
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-07 23:28:50 +0600

    refactor: Remove unused CLI code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 extern crate dotenv;
 use askitty;
-use std::env;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     dotenv::dotenv().ok();
-
-    let config = askitty::cli::Config::new(env::args()).unwrap_or_else(|_err| {
-        eprintln!("Problem parsing arguments");
-        std::process::exit(1);
-    });
-
-    askitty::run(config).await.unwrap_or_else(|err| {
-        eprintln!("Application error: {}", err);
-        std::process::exit(1);
-    });
+    askitty::run();
 }
