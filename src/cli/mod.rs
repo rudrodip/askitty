36f05b120064923c9b13de94c5167a345acef225
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-08 08:27:46 +0600

    refactor: Update run function to use async/await syntax for LLM and IM clients

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,58 +1,39 @@
 pub mod config;
 
-use std::fs;
-use config::Config;
-use crate::ai::{llm::{client::Client as LLMClient, traits::LLM}, im::{client::Client as IMClient, traits::IM}};
+use crate::ai::{
+    im::{client::Client as IMClient, traits::IM},
+    llm::{client::Client as LLMClient, traits::LLM},
+};
 use crate::types::llm::Message;
+use config::Config;
+use std::{error::Error, fs};
 use termimad;
 
-pub fn run(config: Config) {
+pub async fn run(config: Config) -> Result<(), Box<dyn Error>> {
     match config.command {
         config::Command::Help => {
             print_help();
+            Ok(())
         }
         config::Command::Version => {
             print_version();
+            Ok(())
         }
         config::Command::Message(message) => {
-            let client = LLMClient::new();
-            let chats = vec![Message { role: String::from("user"), content: message }];
+            let client = LLMClient::new().unwrap();
+            let chats = vec![Message {
+                role: String::from("user"),
+                content: message,
+            }];
 
-            match client {
-                Ok(client) => {
-                    let response = client.completion(chats);
-                    match response {
-                        Ok(response) => {
-                            termimad::print_text(&response);
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                }                
-            }
+            let response = client.completion(chats).await.unwrap();
+            termimad::print_text(&response);
+
+            Ok(())
         }
         config::Command::Imagine(prompt) => {
-            let client = IMClient::new();
-            match client {
-                Ok(client) => {
-                    let response = client.generate(prompt);
-                    match response {
-                        Ok(_) => {
-                            println!("Image generated successfully");
-                        }
-                        Err(e) => {
-                            println!("Error: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                }
-            }
+            let client = IMClient::new().unwrap();
+            Ok(client.generate(prompt).await.unwrap())
         }
     }
 }
@@ -68,8 +49,13 @@ fn print_help() {
 }
 
 fn print_version() {
-  let contents = fs::read_to_string("Cargo.toml")
-      .expect("Something went wrong reading the file");
-  let version = contents.lines().nth(2).unwrap().split(" = ").nth(1).unwrap();
-  println!("Version: {}", version);
+    let contents = fs::read_to_string("Cargo.toml").expect("Something went wrong reading the file");
+    let version = contents
+        .lines()
+        .nth(2)
+        .unwrap()
+        .split(" = ")
+        .nth(1)
+        .unwrap();
+    println!("Version: {}", version);
 }
