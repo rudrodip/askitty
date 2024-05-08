bf8782ae85721335ea8c7f5bac5d175eacf104fe
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-08 08:50:11 +0600

    error types updated

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,10 +1,12 @@
-use std::error::Error;
-use std::fmt;
+use std::{error::Error, fmt, io};
 
 #[derive(Debug)]
 pub enum LLMError {
     APIError(String),
     SerializationError(String),
+    ReqwestError(reqwest::Error),
+    IOError(io::Error),
+    Other(String),
 }
 
 impl fmt::Display for LLMError {
@@ -12,16 +14,41 @@ impl fmt::Display for LLMError {
         match self {
             LLMError::APIError(msg) => write!(f, "API Error: {}", msg),
             LLMError::SerializationError(msg) => write!(f, "Serialization Error: {}", msg),
+            LLMError::ReqwestError(err) => write!(f, "Reqwest Error: {}", err),
+            LLMError::IOError(err) => write!(f, "IO Error: {}", err),
+            LLMError::Other(msg) => write!(f, "Other Error: {}", msg),
         }
     }
 }
 
 impl Error for LLMError {}
 
+impl From<reqwest::Error> for LLMError {
+    fn from(err: reqwest::Error) -> Self {
+        LLMError::ReqwestError(err)
+    }
+}
+
+impl From<serde_json::Error> for LLMError {
+    fn from(err: serde_json::Error) -> Self {
+        LLMError::SerializationError(err.to_string())
+    }
+}
+
+impl From<io::Error> for LLMError {
+    fn from(err: io::Error) -> Self {
+        LLMError::IOError(err)
+    }
+}
+
 #[derive(Debug)]
 pub enum ImageGenError {
     APIError(String),
     DownloadError(String),
+    ReqwestError(reqwest::Error),
+    SerializationError(String),
+    IOError(io::Error),
+    Other(String),
 }
 
 impl fmt::Display for ImageGenError {
@@ -29,8 +56,30 @@ impl fmt::Display for ImageGenError {
         match self {
             ImageGenError::APIError(msg) => write!(f, "API Error: {}", msg),
             ImageGenError::DownloadError(msg) => write!(f, "Download Error: {}", msg),
+            ImageGenError::ReqwestError(err) => write!(f, "Reqwest Error: {}", err),
+            ImageGenError::SerializationError(msg) =>  write!(f, "Serialization Error: {}", msg),
+            ImageGenError::IOError(err) => write!(f, "IO Error: {}", err),
+            ImageGenError::Other(msg) => write!(f, "Other Error: {}", msg),
         }
     }
 }
 
 impl Error for ImageGenError {}
+
+impl From<reqwest::Error> for ImageGenError {
+    fn from(err: reqwest::Error) -> Self {
+        ImageGenError::ReqwestError(err)
+    }
+}
+
+impl From<serde_json::Error> for ImageGenError {
+    fn from(err: serde_json::Error) -> Self {
+        ImageGenError::SerializationError(err.to_string())
+    }
+}
+
+impl From<io::Error> for ImageGenError {
+    fn from(err: io::Error) -> Self {
+        ImageGenError::IOError(err)
+    }
+}
