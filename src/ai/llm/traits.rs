86929e4c4e7165df00b56d93c39a256cfd9893fe
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-08 08:30:42 +0600

    refactor: Update LLM trait to use async/await syntax for the completion method

--- src/ai/llm/traits.rs
+++ src/ai/llm/traits.rs
@@ -1,8 +1,13 @@
-use crate::types::llm::*;
 use crate::errors::LLMError;
+use crate::types::llm::*;
 use std::future::Future;
 
 pub trait LLM {
-  fn new() -> Result<Self, &'static str> where Self: Sized;
-  fn completion(&self, chats: Vec<Message>) -> impl Future<Output = Result<String, LLMError>> + Send;
+    fn new() -> Result<Self, &'static str>
+    where
+        Self: Sized;
+    fn completion(
+        &self,
+        chats: Vec<Message>,
+    ) -> impl Future<Output = Result<String, LLMError>> + Send;
 }
