a7a3cf95cbc73c860bcac472f1e411b7fedcad47
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-08 09:16:37 +0600

    refactor: Improve error management and organization

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,58 +1,35 @@
 pub mod config;
+pub mod utils;
 
-use crate::ai::{
-    im::{client::Client as IMClient, traits::IM},
-    llm::{client::Client as LLMClient, traits::LLM},
-};
+use crate::ai::{im::traits::IM, llm::traits::LLM};
 use crate::types::llm::Message;
 use config::Config;
-use std::{error::Error, fs};
+use std::error::Error;
 use termimad;
 
-pub async fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub async fn run<L, I>(config: Config) -> Result<(), Box<dyn Error>>
+where
+    L: LLM + 'static,
+    I: IM + 'static,
+{
     match config.command {
-        config::Command::Help => print_help(),
-        config::Command::Version => print_version(),
+        config::Command::Help => utils::print_help(),
+        config::Command::Version => utils::print_version(),
         config::Command::Message(message) => {
-            let client = LLMClient::new().unwrap();
+            let client = L::new()?;
             let chats = vec![Message {
                 role: String::from("user"),
                 content: message,
             }];
 
-            let response = client.completion(chats).await.unwrap();
+            let response = client.completion(chats).await?;
             termimad::print_text(&response);
 
             Ok(())
         }
         config::Command::Imagine(prompt) => {
-            let client = IMClient::new().unwrap();
-            Ok(client.generate(prompt).await.unwrap())
+            let client = I::new()?;
+            Ok(client.generate(prompt).await?)
         }
     }
-}
-
-fn print_help() -> Result<(), Box<dyn Error>> {
-    println!("Usage: askitty [FLAG] [MESSAGE]");
-    println!();
-    println!("Flags:");
-    println!("  -h, --help       Display help message");
-    println!("  -v, --version    Display version");
-    println!("  -m, --message    Message to send to the model");
-    println!("  -i, --imagine    Generate image from text");
-
-    Ok(())
-}
-
-fn print_version() -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string("Cargo.toml").expect("Something went wrong reading the file");
-    let version = contents
-        .lines()
-        .nth(2)
-        .unwrap()
-        .split(" = ")
-        .nth(1)
-        .unwrap();
-    println!("Version: {}", version);
-    Ok(())
 }
