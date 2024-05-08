b23aad6a45162dc53c1133071596ea57253c0267
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-08 08:27:25 +0600

    refactor: Update main function to use Config for configuration

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 extern crate dotenv;
 use askitty;
+use askitty::cli::config::Config;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv::dotenv().ok();
-    askitty::run();
+
+    let config = Config::new().unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    if let Err(err) = askitty::run(config).await {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
 }
