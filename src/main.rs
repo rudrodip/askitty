5c7817f20041d1e45b1113ec8c3f696dcfa094e0
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-09 16:04:57 +0600

    refactor: Add lazy loading for images
    Add lazy loading functionality to improve page load performance by loading images only when they are visible on the screen.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 extern crate dotenv;
-use askitty;
 use askitty::cli::config::Config;
+use askitty::setup;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+    let storage = setup::initialize_storage();
 
     let config = Config::new().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
-    if let Err(err) = askitty::run(config).await {
+    if let Err(err) = askitty::run(config, storage).await {
         eprintln!("Error: {}", err);
         std::process::exit(1);
     }
