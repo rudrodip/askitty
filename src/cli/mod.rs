9937948535c746752fad576b3e6744e3c66adb59
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-09 16:05:06 +0600

    refactor: Add chat history storage and update CLI run function
    Add a new storage module and setup function for initializing the chat history storage. Modify the CLI run function to use the chat history storage to store and retrieve chat messages. This improves the functionality of the chat feature by allowing the user to view previous chat messages and continue the conversation seamlessly.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,33 +3,50 @@ pub mod utils;
 
 use crate::ai::{im::traits::IM, llm::traits::LLM};
 use crate::types::llm::Message;
+use crate::storage::Storage;
 use config::Config;
+use std::collections::VecDeque;
 use std::error::Error;
 use termimad;
 
-pub async fn run<L, I>(config: Config) -> Result<(), Box<dyn Error>>
+pub async fn run<L, I>(config: Config, storage: impl Storage) -> Result<(), Box<dyn Error>>
 where
     L: LLM + 'static,
     I: IM + 'static,
 {
+    let llm_client = L::new()?;
+    let im_client = I::new()?;
+    let mut chat_history: VecDeque<Message> = storage
+        .get("CHAT_HISTORY")
+        .unwrap_or_default()
+        .unwrap_or_default();
+
     match config.command {
         config::Command::Help => utils::print_help(),
         config::Command::Version => utils::print_version(),
         config::Command::Message(message) => {
-            let client = L::new()?;
-            let chats = vec![Message {
+            let chat = Message {
                 role: String::from("user"),
                 content: message,
-            }];
+            };
+            chat_history.push_back(chat);
+            let chats: Vec<Message> = chat_history.iter().cloned().collect();
+            let response = llm_client.completion(chats).await?;
 
-            let response = client.completion(chats).await?;
+            chat_history.push_back(Message {
+                role: String::from("assistant"),
+                content: response.clone(),
+            });
+
+            storage
+                .set("CHAT_HISTORY", &chat_history)
+                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
             termimad::print_text(&response);
-
             Ok(())
         }
         config::Command::Imagine(prompt) => {
-            let client = I::new()?;
-            Ok(client.generate(prompt).await?)
+            Ok(im_client.generate(prompt).await?)
         }
     }
 }
