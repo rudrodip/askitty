5bbdb8ea391906cae3548108d2b5103bcd4633c6
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-09 16:05:12 +0600

    refactor: Add BincodeError variant to StorageError
    This commit adds a new variant, `BincodeError`, to the `StorageError` enum in the `errors` module. This variant represents errors that occur during the serialization and deserialization process using the `bincode` crate. The addition of this variant enhances the error handling capabilities of the storage module.

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,4 +1,5 @@
 use std::{error::Error, fmt, io};
+use bincode::Error as BincodeError;
 
 #[derive(Debug)]
 pub enum LLMError {
@@ -81,5 +82,52 @@ impl From<serde_json::Error> for ImageGenError {
 impl From<io::Error> for ImageGenError {
     fn from(err: io::Error) -> Self {
         ImageGenError::IOError(err)
+    }
+}
+
+#[derive(Debug)]
+pub enum StorageError {
+    IOError(io::Error),
+    SerializationError(String),
+    SledError(sled::Error),
+    BincodeError(BincodeError),
+    Other(String),
+}
+
+impl fmt::Display for StorageError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StorageError::IOError(err) => write!(f, "IO Error: {}", err),
+            StorageError::SerializationError(msg) => write!(f, "Serialization Error: {}", msg),
+            StorageError::SledError(err) => write!(f, "Sled Error: {}", err),
+            StorageError::BincodeError(err) => write!(f, "Bincode Error: {}", err),
+            StorageError::Other(msg) => write!(f, "Other Error: {}", msg),
+        }
+    }
+}
+
+impl Error for StorageError {}
+
+impl From<io::Error> for StorageError {
+    fn from(err: io::Error) -> Self {
+        StorageError::IOError(err)
+    }
+}
+
+impl From<serde_json::Error> for StorageError {
+    fn from(err: serde_json::Error) -> Self {
+        StorageError::SerializationError(err.to_string())
+    }
+}
+
+impl From<sled::Error> for StorageError {
+    fn from(err: sled::Error) -> Self {
+        StorageError::SledError(err)
+    }
+}
+
+impl From<BincodeError> for StorageError {
+    fn from(err: BincodeError) -> Self {
+        StorageError::BincodeError(err)
     }
 }
