33be3b90b1f3565fdc66c6c8ff03b7b6b49b3c02
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-10 11:01:41 +0600

    refactor: Add session-related commands to CLI config

--- src/cli/utils.rs
+++ src/cli/utils.rs
@@ -1,3 +1,5 @@
+use crate::types::llm::Session;
+use crate::Storage;
 use std::{error::Error, fs};
 
 pub fn print_help() -> Result<(), Box<dyn Error>> {
@@ -28,4 +30,167 @@ pub fn read_line() -> Result<String, Box<dyn Error>> {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input)?;
     Ok(input.trim().to_string())
+}
+
+pub fn find_latest_session(storage: &impl Storage) -> Option<Session> {
+    let mut latest_session: Option<Session> = None;
+    let mut latest_timestamp: i64 = 0;
+
+    if let Ok(session_ids) = storage.iter_keys() {
+        for session_id in session_ids {
+            if let Ok(Some(session)) = storage.get::<Session>(&session_id) {
+                if session.created_at > latest_timestamp {
+                    latest_session = Some(session.clone());
+                    latest_timestamp = session.created_at
+                }
+            }
+        }
+    }
+
+    latest_session
+}
+
+pub fn find_all_sessions(storage: &impl Storage) -> Result<Vec<Session>, Box<dyn Error>> {
+    let mut sessions: Vec<Session> = vec![];
+
+    if let Ok(session_ids) = storage.iter_keys() {
+        for session_id in session_ids {
+            if let Ok(Some(session)) = storage.get::<Session>(&session_id) {
+                sessions.push(session.clone());
+            }
+        }
+    }
+
+    Ok(sessions)
+}
+
+pub fn print_text(text: &str) {
+    termimad::print_text(text);
+}
+
+pub fn show_all_sessions(storage: &impl Storage) -> Result<(), Box<dyn Error>> {
+    match find_all_sessions(storage) {
+        Ok(sessions) => {
+            for session in sessions {
+                println!("{}", session);
+            }
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
+}
+
+pub fn show_session(storage: &impl Storage, session_id: &str) -> Result<(), Box<dyn Error>> {
+    match storage.get::<Session>(&session_id) {
+        Ok(Some(session)) => {
+            println!("{}", session);
+            println!("System prompt: {}", session.system_prompt);
+
+            // print the history
+            for message in session.history {
+                println!("{}", message);
+            }
+            Ok(())
+        }
+        Ok(None) => {
+            println!("Session not found");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn delete_session(storage: &impl Storage, session_id: &str) -> Result<(), Box<dyn Error>> {
+    match storage.delete(&session_id) {
+        Ok(_) => {
+            println!("Session deleted");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn clear_sessions(storage: &impl Storage) -> Result<(), Box<dyn Error>> {
+    match storage.clear() {
+        Ok(_) => {
+            println!("All sessions deleted");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn set_global_system_prompt(storage: &impl Storage, prompt: &str) -> Result<(), Box<dyn Error>> {
+    storage
+        .set("GLOBAL_SYSTEM_PROMPT", &prompt)
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+    println!("Global system prompt set to: {}", prompt);
+    Ok(())
+}
+
+pub fn set_session_system_prompt(
+    storage: &impl Storage,
+    session_id: &str,
+    prompt: &str,
+) -> Result<(), Box<dyn Error>> {
+    match storage.get::<Session>(&session_id) {
+        Ok(Some(mut session)) => {
+            session.system_prompt = prompt.to_string();
+            storage
+                .set(&session_id, &session)
+                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+            println!("Session system prompt set to: {}", prompt);
+            Ok(())
+        }
+        Ok(None) => {
+            println!("Session not found");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn show_global_system_prompt(storage: &impl Storage) -> Result<(), Box<dyn Error>> {
+    match storage.get::<String>("GLOBAL_SYSTEM_PROMPT") {
+        Ok(Some(prompt)) => {
+            println!("Global system prompt: {}", prompt);
+            Ok(())
+        }
+        Ok(None) => {
+            println!("Global system prompt not set");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn clear_global_system_prompt(storage: &impl Storage) -> Result<(), Box<dyn Error>> {
+    match storage.set("GLOBAL_SYSTEM_PROMPT", &String::new()) {
+        Ok(_) => {
+            println!("Global system prompt cleared");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
+}
+
+pub fn delete_session_system_prompt(
+    storage: &impl Storage,
+    session_id: &str,
+) -> Result<(), Box<dyn Error>> {
+    match storage.get::<Session>(&session_id) {
+        Ok(Some(mut session)) => {
+            session.system_prompt = String::new();
+            storage
+                .set(&session_id, &session)
+                .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+            println!("Session system prompt cleared");
+            Ok(())
+        }
+        Ok(None) => {
+            println!("Session not found");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e) as Box<dyn Error>),
+    }
 }
