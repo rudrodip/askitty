94afbae5eac555b93eaf19589907609ee6ef03fc
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-24 09:35:49 +0600

    updated image gen types

--- src/types/im.rs
+++ src/types/im.rs
@@ -1,34 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct ImageGenResponse {
-    pub id: String,
-    pub model: String,
-    pub version: String,
-    pub input: Input,
-    pub logs: String,
-    pub output: Option<Vec<String>>,
-    pub error: Option<String>,
-    pub status: String,
-    pub created_at: String,
-    pub started_at: Option<String>,
-    pub completed_at: Option<String>,
-    pub urls: Urls,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ImageResponse {
+    pub created: i64,
+    pub data: Vec<ImageData>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Input {
-    pub guidance_scale: f32,
-    pub height: i32,
-    pub num_inference_steps: i32,
-    pub num_outputs: i32,
-    pub prompt: String,
-    pub scheduler: String,
-    pub width: i32,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Urls {
-    pub cancel: String,
-    pub get: String,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ImageData {
+    pub revised_prompt: String,
+    pub url: String,
 }
